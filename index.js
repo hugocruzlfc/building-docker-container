d49dedfed74294eb64a3fd23e656dc87abd41959
hde_la_torre <hugo.de_la_torre@sita.aero>
2023-03-03 12:03:47 +0100

    Upload project

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const Animal = mongoose.model(
+  "Animal",
+  new mongoose.Schema({
+    tipo: String,
+    estado: String,
+  })
+);
+
+const app = express();
+
+mongoose.connect(
+  "mongodb://admin:admin@localhost:27017/mongodocker?authSource=admin"
+);
+
+app.get("/", async (_req, res) => {
+  console.log("listando... chanchitos...");
+  const animales = await Animal.find();
+  console.log(animales);
+  return res.send(animales);
+});
+app.get("/crear", async (_req, res) => {
+  console.log("creando...");
+  await Animal.create({ tipo: "Chanchito", estado: "Feliz" });
+  return res.send("ok");
+});
+
+app.listen(3000, () => console.log("listening..."));
